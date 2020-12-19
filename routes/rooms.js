c4cdb778dcabf7e41553788c3988fac6fbb2f9a6
“David <davidestebandeveloper@gmail.com>
2020-12-19 17:44:47 -0300

    fix bug

--- routes/rooms.js
+++ routes/rooms.js
@@ -8,6 +8,10 @@ const { check } = require('express-validator');
 const { createRoom } = require('../controllers/rooms');
 const { validarCampos } = require('../middlewares/validar-campos');
 const { validateJWT } = require('../middlewares/validar-jwt');
+const { renewToken } = require('../controllers/auth');
+
+
+
 const router = Router();
 
 
