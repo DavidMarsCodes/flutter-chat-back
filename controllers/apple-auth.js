42408c9f2e3fd5aa0a1f2c27f7f5b9e7bf2c9a33
“David <davidestebandeveloper@gmail.com>
2021-01-06 12:31:46 -0300

    apple signin beta

--- controllers/apple-auth.js
+++ controllers/apple-auth.js
@@ -18,9 +18,9 @@ const callbackAppleAuth = async (request, response) => {
       ).toString()}#Intent;package=${
         process.env.ANDROID_PACKAGE_IDENTIFIER
       };scheme=signinwithapple;end`;
-    
+
       console.log(`Redirecting to ${redirect}`);
-    
+
       response.redirect(307, redirect);
 
 }
@@ -43,28 +43,30 @@ const auth = new AppleAuth(
         fs.readFileSync('./keys/keysignin.p8').toString(),
         "text"
       );
-    
+      console.log('auth', auth);
+
       console.log(request.query);
-    
+
+
       const accessToken = await auth.accessToken(request.query.code);
-    
+
       const idToken = jwt.decode(accessToken.id_token);
-    
+
       const userID = idToken.sub;
-    
+
       console.log(idToken);
-    
+
       // `userEmail` and `userName` will only be provided for the initial authorization with your app
       const userEmail = idToken.email;
       const userName = `${request.query.firstName} ${request.query.lastName}`;
-    
+
       // 👷🏻‍♀️ TODO: Use the values provided create a new session for the user in your system
       const sessionID = `NEW SESSION ID for ${userID} / ${userEmail} / ${userName}`;
-    
-      console.log(`sessionID = ${sessionID}`);
-    
-      response.json({ sessionId: sessionID });
 
+      console.log(`sessionID = ${sessionID}`);
+
+      response.json({ sessionId: sessionID });
+/* 
 
 console.log(req.body);
     const tokenGoogle = req.body.token;
@@ -87,7 +89,7 @@ console.log(req.body);
 
     var newUsername = email.split('@')[0];
 
-    
+
 
 console.log(email)
 
@@ -104,15 +106,15 @@ console.log(email)
     const user = await User.findOne({ email });
     if( user ) {
 
-       
 
-        const profileFind = await 
+
+        const profileFind = await
         Profile.findOne({user: user.id})
         .populate('user')
 
 
         const profile = {
-        
+
             name: profileFind.name,
             lastName: profileFind.lastName,
             imageHeader: profileFind.imageHeader,
@@ -123,37 +125,37 @@ console.log(email)
               uid: user.id,
               email: user.email,
               username:  user.username,
-           
+
             },
             createdAt: profileFind.createdAt,
             updatedAt:profileFind.updatedAt
-           
+
           }
 
           const token = await generateJWT( user.id );
 
-      
-     
+
+
     return res.json({
             ok: true,
             profile,
             token
         })
 
-      
+
     }
 
     const newUser = new User( {email: email, password: id, username: newUsername, isAuthGoogle: true });
 
 
 
-   
+
     // Encriptar contraseña
     const salt = bcrypt.genSaltSync();
     newUser.password = bcrypt.hashSync( id, salt );
 
     await newUser.save();
-   
+
     const token = await generateJWT( newUser.id );
 
 
@@ -162,14 +164,14 @@ console.log(email)
 
     await profileNew.save()
 
-    const profileFind = await 
+    const profileFind = await
     Profile.findOne({user: newUser.id})
 
     console.log('profileFind!!', profileFind)
 
     // Generar mi JWT
     const profile = {
-        
+
         name: profileFind.name,
         lastName: profileFind.lastName,
         imageHeader: profileFind.imageHeader,
@@ -180,20 +182,20 @@ console.log(email)
           uid: newUser.id,
           email: newUser.email,
           username:  newUser.username,
-       
+
         },
         createdAt: profileFind.createdAt,
         updatedAt:profileFind.updatedAt
-       
+
       }
 
       console.log('profile!!', profile)
+ */
 
- 
     res.json({
         ok: true,
-        profile,
-        token
+      //  profile,
+       // token
     })
 }
 
