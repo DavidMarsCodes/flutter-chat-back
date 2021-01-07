aac16d7cfabfa238e58770d7db6b5ca3f9c19112
“David <davidestebandeveloper@gmail.com>
2021-01-07 14:25:47 -0300

    edit position room api

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -116,7 +116,7 @@ const editPositionByRoom = async (req, res = response ) => {
 
       const room =  await Room.updateOne(
             {
-              id: roomId
+              _id: roomId
             },
             {
               $set: {
