bf4987ad55ea512027aee73d1f62c85f8ddedf2d
“David <davidestebandeveloper@gmail.com>
2021-01-13 20:22:18 -0300

    api aws upload image  byte buffer

--- controllers/aws.js
+++ controllers/aws.js
@@ -5,7 +5,7 @@ const Profile = require('../models/profile');
 
 const uploadAvatar = async (req, res = response ) => {
 
-    console.log('biody** ',req.body)
+    console.log('biody** ',req)
 
     try {
    
