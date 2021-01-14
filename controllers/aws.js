a8736a271358f0f593bc5574437cf1051a7c56ca
“David <davidestebandeveloper@gmail.com>
2021-01-13 22:29:50 -0300

    api aws upload image  byte buffer

--- controllers/aws.js
+++ controllers/aws.js
@@ -14,7 +14,7 @@ const uploadAvatar = async (req, res = response ) => {
 
     const S3_BUCKET = process.env.Bucket;
   
-    const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+    const s3 = new AWS.S3();
     const fileName = req.files.file.name;
 
     console.log(fileName )
