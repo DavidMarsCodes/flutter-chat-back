53b89ed1eb1cb840dcd8016fca174b401509d5a9
“David <davidestebandeveloper@gmail.com>
2021-01-31 12:36:20 -0300

    upload image plant api

--- controllers/aws.js
+++ controllers/aws.js
@@ -1,40 +1,48 @@
 require('dotenv').config()
 var aws = require('aws-sdk');
 const Profile = require('../models/profile');
+const Plant = require('../models/plant');
 
 var fs =require('fs');
 var data = fs.readFileSync('./aws/keys.json', 'utf8');
 var keys = JSON.parse(data);
 
-console.log('aws', keys);
+
+
+
+
+
 
 const uploadAvatar = async (req, res = response ) => {
 
+
+    
    
         console.log("req ##", req.headers.uid);  
 
     try {
-   
+
+
+const S3_BUCKET = keys.Bucket;
+const s3 = new aws.S3();
+const fileName = req.files.file.name;
+const fileType = req.files.file.mimetype;
+//const fileName = String(Date.now()) + '.' + fileType;
+const folder = 'avatar';
+const buffer = req.files.file.data;
+
+console.log(fileName,fileType )
+const s3Params = {
+    Bucket: S3_BUCKET + '/' + folder,
+    Key: fileName,
+    //Expires: 500,
+    Body: buffer,
+    ContentType: fileType,
+   ACL: 'public-read'
+}
 
    
 
-    const S3_BUCKET = keys.Bucket;
-    const s3 = new aws.S3();
-    const fileName = req.files.file.name;
-    const fileType = req.files.file.mimetype;
-    //const fileName = String(Date.now()) + '.' + fileType;
-    const folder = 'avatar';
-    const buffer = req.files.file.data;
-
-    console.log(fileName,fileType )
-    const s3Params = {
-        Bucket: S3_BUCKET + '/' + folder,
-        Key: fileName,
-        //Expires: 500,
-        Body: buffer,
-        ContentType: fileType,
-       ACL: 'public-read'
-    }
 
     console.log('s3Params', s3Params)
 
@@ -93,8 +101,6 @@ const uploadHeader = async (req, res = response ) => {
 try {
 
 
-
-
 const S3_BUCKET = keys.Bucket;
 const s3 = new aws.S3();
 const fileName = req.files.file.name;
@@ -112,6 +118,8 @@ const s3Params = {
     ContentType: fileType,
    ACL: 'public-read'
 }
+
+
 
 console.log('s3Params', s3Params)
 
@@ -162,7 +170,143 @@ s3.upload(s3Params, async (err, data) => {
 
 }
 
+
+const uploadCoverPlant = async (req, res = response ) => {
+
+   
+    console.log("req ##", req.headers.uid);  
+
+try {
+
+
+const S3_BUCKET = keys.Bucket;
+const s3 = new aws.S3();
+const fileName = req.files.file.name;
+const fileType = req.files.file.mimetype;
+//const fileName = String(Date.now()) + '.' + fileType;
+const folder = 'coverPlant';
+const buffer = req.files.file.data;
+
+console.log(fileName,fileType )
+const s3Params = {
+    Bucket: S3_BUCKET + '/' + folder,
+    Key: fileName,
+    //Expires: 500,
+    Body: buffer,
+    ContentType: fileType,
+   ACL: 'public-read'
+}
+
+
+
+console.log('s3Params', s3Params)
+
+s3.upload(s3Params, async (err, data) => {
+    
+    if (err) {
+        console.log(err);
+        res.json({ success: false, error: err })
+    }
+
+    const returnData = {
+        signedRequest: data,
+        url: `https://${S3_BUCKET}.s3.sa-east-1.amazonaws.com/${folder}/${fileName}`
+    };
+
+    console.log(returnData)
+
+    const uid = req.headers.uid;
+
+    console.log('UID: ', uid);
+
+  const  profileUpdate = await Plant.updateOne(
+        {
+            user: uid,
+            room: roomId
+        },
+        {
+            $set: {
+                imageHeader: returnData.url,
+          
+                
+
+            }
+        }
+    );
+
+
+
+    console.log('profileUpdate', profileUpdate)  
+
+    res.json({ ok: true, url: returnData.url  });
+
+});
+
+    
+} catch (error) {
+    
+}
+
+}
+
+const uploadCoverPlant = async (req, res = response ) => {
+
+   
+    console.log("req ##", req.headers);  
+
+try {
+
+
+const S3_BUCKET = keys.Bucket;
+const s3 = new aws.S3();
+const fileName = req.files.file.name;
+const fileType = req.files.file.mimetype;
+//const fileName = String(Date.now()) + '.' + fileType;
+const folder = 'coverPlant';
+const buffer = req.files.file.data;
+
+console.log(fileName,fileType )
+const s3Params = {
+    Bucket: S3_BUCKET + '/' + folder,
+    Key: fileName,
+    //Expires: 500,
+    Body: buffer,
+    ContentType: fileType,
+   ACL: 'public-read'
+}
+
+
+
+console.log('s3Params', s3Params)
+
+s3.upload(s3Params, async (err, data) => {
+    
+    if (err) {
+        console.log(err);
+        res.json({ success: false, error: err })
+    }
+
+    const returnData = {
+        signedRequest: data,
+        url: `https://${S3_BUCKET}.s3.sa-east-1.amazonaws.com/${folder}/${fileName}`
+    };
+
+    console.log(returnData)
+
+
+    res.json({ ok: true, url: returnData.url  });
+
+});
+
+    
+} catch (error) {
+    
+}
+
+}
+
 module.exports = {
+    uploadCoverPlant,
     uploadAvatar,
     uploadHeader
 }
--- routes/aws.js
+++ routes/aws.js
@@ -2,7 +2,7 @@
 const { Router } = require('express');
 const { validateJWT } = require('../middlewares/validar-jwt');
 
-const {   uploadAvatar, uploadHeader } = require('../controllers/aws');
+const {   uploadAvatar, uploadHeader , uploadCoverPlant} = require('../controllers/aws');
 
 
 
@@ -10,6 +10,9 @@ const router = Router();
 
 router.post("/upload/avatar", validateJWT, uploadAvatar ) 
 router.post("/upload/header", validateJWT, uploadHeader ) 
+
+router.post("/upload/cover-plant", validateJWT, uploadCoverPlant )
+
 
   
 
