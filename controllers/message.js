0465069549c00549ec302fc5aecc9d51bd8ccece
“David <davidestebandeveloper@gmail.com>
2021-02-18 15:20:26 -0300

    api approve subscription

--- controllers/message.js
+++ controllers/message.js
@@ -48,16 +48,18 @@ const getProfilesChat = async (req, res) => {
 
         const isClub = myprofile.isClub;
 
-        console.log('Club**!!! ', isClub);
 
         messagesUnique = [];
         messagesUnique = Object.values(messages.reduce((acc, cur) => Object.assign(acc, { [cur.by.toString()]: cur }), {}));
 
 
 
+        console.log('messagesUnique', messagesUnique);
         const profiles = [];
 
         const promises = messagesUnique.map((obj) =>
+
+
 
 
             new Promise((resolve, reject) => {
