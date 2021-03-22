a7feccabb94ae1e0816007730e3e612d435e6953
“David <davidestebandeveloper@gmail.com>
2021-03-22 16:03:31 -0300

    product principal like by user auth

--- controllers/catalogo.js
+++ controllers/catalogo.js
@@ -824,6 +824,24 @@ const getMyCatalogosProducts = async (req, res = response) => {
 
 
 
+                Promise.all(promisesFavorite)
+                    .then((resolve) => {
+
+
+
+
+                        const catalogosProductsPosition = catalogosProducts.sort((a, b) => (a.position > b.position) ? 1 : -1)
+                        console.log('catalogosProductsPosition', catalogosProductsPosition);
+
+                        return res.json({
+                            ok: true,
+
+                            catalogosProducts: catalogosProductsPosition
+                        })
+
+
+
+                    });
 
 
 
