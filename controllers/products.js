3935ff284358cc1f0a6091ceac628912317b0c3d
“David <davidestebandeveloper@gmail.com>
2021-03-23 20:06:45 -0300

    get products by user club

--- controllers/products.js
+++ controllers/products.js
@@ -371,8 +371,10 @@ const getLastProducts = async (req, res = response) => {
 
                                                                                 else if (catalogo.privacity == '2') {
 
+                                                                                    const isSubsOrClub = (isClub) ? false : subscribeApproved && subscribeActive;
 
-                                                                                    if (subscribeApproved && subscribeActive) {
+
+                                                                                    if (isSubsOrClub) {
 
                                                                                         productsProfiles.push(productProfile);
                                                                                         resolve();
