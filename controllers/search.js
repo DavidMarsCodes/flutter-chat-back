6e6f3f7dc6f1f99d19bc5ddb6cb4c7f5b836d618
“David <davidestebandeveloper@gmail.com>
2021-03-24 12:33:25 -0300

    get profiles search isClub

--- controllers/search.js
+++ controllers/search.js
@@ -7,73 +7,75 @@ const getSearchPrincipalByQuery = async (req, res = response) => {
 
 
     const QueryRegex = new RegExp(req.params.query);
-   
-    
+
+
     try {
 
 
-    const resultFindArray = await 
+        const resultFindArray = await
 
-        Profile.find({'name': {$regex: QueryRegex, $options: 'i'}})
-        .limit(50)
-        .populate('user');
+            Profile.find({ 'name': { $regex: QueryRegex, $options: 'i' } })
+                .limit(50)
+                .populate('user');
 
 
-       // .sort('-online')
+        // .sort('-online')
 
-   // Profile.find({ $and: [ { $or: [{name: QueryRegex },{user.username: regex}] }, {category: value.category}, {city:value.city} ] } )
+        // Profile.find({ $and: [ { $or: [{name: QueryRegex },{user.username: regex}] }, {category: value.category}, {city:value.city} ] } )
 
-    const profiles = [];
+        const profiles = [];
 
-   // console.log('profilesFind', profilesFind);
+        // console.log('profilesFind', profilesFind);
 
-   resultFindArray.map((item) => {
+        resultFindArray.map((item) => {
 
-       
 
-       
 
-        const profile = {
-            name: item.name,
-            lastName: item.lastName,
-            about: item.about,
-            imageHeader: item.imageHeader,
-            imageAvatar: item.imageAvatar,
-            id: item._id,
-            user: {
-                online: item.user.online,
-                uid: item.user._id,
-                email: item.user.email,
-                username: item.user.username,
+            if (item.isClub) {
 
-            },
+                const profile = {
+                    name: item.name,
+                    lastName: item.lastName,
+                    about: item.about,
+                    imageHeader: item.imageHeader,
+                    imageAvatar: item.imageAvatar,
+                    id: item._id,
+                    user: {
+                        online: item.user.online,
+                        uid: item.user._id,
+                        email: item.user.email,
+                        username: item.user.username,
 
-            messageDate: item.updatedAt,
-            isClub: item.isClub,
-            subscribeActive: item.subscribeActive,
-            subscribeApproved: item.subscribeApproved,
-            subId: item._id,
-            isUpload: item.isUpload,
+                    },
 
-            createdAt: item.createdAt,
-            updatedAt: item.updatedAt
+                    messageDate: item.updatedAt,
+                    isClub: item.isClub,
+                    subscribeActive: item.subscribeActive,
+                    subscribeApproved: item.subscribeApproved,
+                    subId: item._id,
+                    isUpload: item.isUpload,
+
+                    createdAt: item.createdAt,
+                    updatedAt: item.updatedAt
+
+                }
+
+                profiles.push(profile);
+
+            }
+
 
         }
+        );
 
-        profiles.push(profile);
 
 
+
+        res.json({
+            ok: true,
+            profiles
+        })
     }
-   );
-    
-
-
-
-    res.json({
-        ok: true,
-        profiles
-    })
-}
 
     catch (error) {
         console.log(error);
