2e00039e40c34a70a3f78c6508f3d23a3a482c83
“David <davidestebandeveloper@gmail.com>
2021-03-27 14:41:37 -0300

    socket notification

--- sockets/socket.js
+++ sockets/socket.js
@@ -3,36 +3,43 @@ const { checkJWT } = require('../helpers/jwt');
 const { userConnect, userDisconnect, saveMessage } = require('../controllers/socket');
 
 // Mensajes de Sockets
-io.on('connect', (client) =>  {
-    const [ valido, uid ] = checkJWT( client.handshake.headers['x-token'] )
-    
+io.on('connect', (client) => {
+    const [valido, uid] = checkJWT(client.handshake.headers['x-token'])
+
     // Verificar autenticación
-    if ( !valido ) { return client.disconnect(); }
+    if (!valido) { return client.disconnect(); }
     console.log('conect', uid)
 
     // Cliente autenticado
-    userConnect( uid );
+    userConnect(uid);
 
 
     // Ingresar al usuario a una sala en particular
     // sala global, client.id, 5f298534ad4169714548b785
-    client.join( uid );
-    
+    client.join(uid);
+
     // Escuchar del cliente el mensaje-personal
-    client.on('personal-message', async( payload ) => {
+    client.on('personal-message', async (payload) => {
         // TODO: Grabar mensaje
 
-        await saveMessage( payload );
-        io.to( payload.for ).emit('personal-message', payload );
+        await saveMessage(payload);
+        io.to(payload.for).emit('personal-message', payload);
     })
 
-    client.on('principal-message', async( payload ) => {
+    client.on('principal-message', async (payload) => {
         // TODO: Grabar mensaje
 
-      
-        io.to( payload.for ).emit('principal-message', payload );
+
+        io.to(payload.for).emit('principal-message', payload);
     })
-    
+
+    client.on('principal-notification', async (payload) => {
+        // TODO: Grabar mensaje
+
+
+        io.to(payload.for).emit('principal-notification', payload);
+    })
+
 
     client.on('disconnect', () => {
 
@@ -40,10 +47,10 @@ io.on('connect', (client) =>  {
         userDisconnect(uid);
     });
 
-    
 
 
-    
+
+
     // client.on('mensaje', ( payload ) => {
     //     console.log('Mensaje', payload);
     //     io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
