bd419ee7538b9936e3ededd838408af8792e2719
“David <davidestebandeveloper@gmail.com>
2021-03-31 12:58:10 -0300

    edit prroduct ad plants selected and return product likes

--- controllers/plant_product.js
+++ controllers/plant_product.js
@@ -85,7 +85,7 @@ const getPlantsByProduct = async (req, res = response) => {
 
             new Promise((resolve, reject) => {
 
-
+                print('plantProduct!!', plantProduct)
 
                 Plant.findById(plantProduct.plant)
                     .then((plant) => {
