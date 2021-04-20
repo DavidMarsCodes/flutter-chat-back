7729004a664c7b0600f8621690fbc7772a973521
“David <davidestebandeveloper@gmail.com>
2021-04-20 18:38:01 -0400

    api products likes dispensary

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ const app = express();
 var fileupload = require("express-fileupload");
 app.use(fileupload());
 
-app.use(express.json());
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
 
 var aws = require('aws-sdk');
