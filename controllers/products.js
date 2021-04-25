64904829e93731c3f166006290b196e141185a57
“David <davidestebandeveloper@gmail.com>
2021-04-25 18:41:19 -0400

    get dispensary active

--- controllers/products.js
+++ controllers/products.js
@@ -512,66 +512,118 @@ const getProductsLikedDispensary = async (req, res = response) => {
 
             new Promise((resolve, reject) => {
 
+                (dispensary) ?
 
-                ProductDispensary.findOne({ product: product._id })
-                    .then((productDispesary) => {
-
-
-
-
-                        const quantityDispensary = (productDispesary && dispensary) ? productDispesary.quantity : 0;
-                        Favorite.findOne({
-                            product: product._id, user: userId
-                        })
-                            .then((favorite) => {
-                                console.log('favorite', favorite)
-
-                                const isLike = (favorite) ? favorite.isLike : false;
-
-                                Favorite.find({
-                                    product: product._id, isLike: true
-                                })
-                                    .then((favorites) => {
-
-                                        console.log('favorites', favorites)
-
-                                        const countLikes = (favorites) ? favorites.length : 0;
+                    ProductDispensary.findOne({ product: product._id, dispensary: dispensary._id })
+                        .then((productDispesary) => {
 
 
 
 
-
-                                        const productObj = {
-
-                                            id: product._id,
-                                            user: product.user,
-                                            name: product.name,
-                                            description: product.description,
-                                            dateCreate: product.createdAt,
-                                            dateUpdate: product.updateAt,
-                                            totalProducts: product.totalProducts,
-                                            coverImage: product.coverImage,
-                                            catalogo: product.catalogo,
-                                            ratingInit: product.ratingInit,
-                                            cbd: product.cbd,
-                                            thc: product.thc,
-                                            isLike: isLike,
-                                            countLikes: countLikes,
-                                            quantityDispensary: quantityDispensary,
-
-
-                                        };
-
-                                        products.push(productObj);
-                                        resolve()
-
-
-
-                                    })
-
+                            const quantityDispensary = (productDispesary) ? productDispesary.quantity : 0;
+                            Favorite.findOne({
+                                product: product._id, user: userId
                             })
+                                .then((favorite) => {
+                                    console.log('favorite', favorite)
 
+                                    const isLike = (favorite) ? favorite.isLike : false;
+
+                                    Favorite.find({
+                                        product: product._id, isLike: true
+                                    })
+                                        .then((favorites) => {
+
+                                            console.log('favorites', favorites)
+
+                                            const countLikes = (favorites) ? favorites.length : 0;
+
+
+
+
+
+                                            const productObj = {
+
+                                                id: product._id,
+                                                user: product.user,
+                                                name: product.name,
+                                                description: product.description,
+                                                dateCreate: product.createdAt,
+                                                dateUpdate: product.updateAt,
+                                                totalProducts: product.totalProducts,
+                                                coverImage: product.coverImage,
+                                                catalogo: product.catalogo,
+                                                ratingInit: product.ratingInit,
+                                                cbd: product.cbd,
+                                                thc: product.thc,
+                                                isLike: isLike,
+                                                countLikes: countLikes,
+                                                quantityDispensary: quantityDispensary,
+
+
+                                            };
+
+                                            products.push(productObj);
+                                            resolve()
+
+
+
+                                        })
+
+                                })
+
+                        }) :
+
+                    Favorite.findOne({
+                        product: product._id, user: userId
                     })
+                        .then((favorite) => {
+                            console.log('favorite', favorite)
+
+                            const isLike = (favorite) ? favorite.isLike : false;
+
+                            Favorite.find({
+                                product: product._id, isLike: true
+                            })
+                                .then((favorites) => {
+
+                                    console.log('favorites', favorites)
+
+                                    const countLikes = (favorites) ? favorites.length : 0;
+
+
+
+
+
+                                    const productObj = {
+
+                                        id: product._id,
+                                        user: product.user,
+                                        name: product.name,
+                                        description: product.description,
+                                        dateCreate: product.createdAt,
+                                        dateUpdate: product.updateAt,
+                                        totalProducts: product.totalProducts,
+                                        coverImage: product.coverImage,
+                                        catalogo: product.catalogo,
+                                        ratingInit: product.ratingInit,
+                                        cbd: product.cbd,
+                                        thc: product.thc,
+                                        isLike: isLike,
+                                        countLikes: countLikes,
+                                        quantityDispensary: 0,
+
+
+                                    };
+
+                                    products.push(productObj);
+                                    resolve()
+
+
+
+                                })
+
+                        })
 
 
 
